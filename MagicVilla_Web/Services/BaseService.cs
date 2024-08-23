86a5e1ac0933e4d0943e67e53f0dbc3e452fe432
kourosh karimi <karimikorosh2000@gmail.com>
2024-08-23 17:20:11 +0330

    Invoke Refresh Token Endpoint

--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -7,6 +7,7 @@ using static MagicVilla_Utility.SD;
 using System.Net.Http.Headers;
 using System.Text;
 using MagicVilla_Web.Models.Dto;
+using Microsoft.AspNetCore.Authentication;
 
 namespace MagicVilla_Web.Services
 {
@@ -17,11 +18,16 @@ namespace MagicVilla_Web.Services
         public APIResponse responseModel { get; set; }
         public IHttpClientFactory httpClient { get; set; }
         private readonly ITokenProvider _tokenProvider;
+        protected readonly string VillaApiUrl;
+        private IHttpContextAccessor _httpContextAccessor;
 
-        public BaseService(IHttpClientFactory httpClient, ITokenProvider tokenProvider)
+        public BaseService(IHttpClientFactory httpClient, ITokenProvider tokenProvider, IConfiguration configuration
+            , IHttpContextAccessor httpContextAccessor)
         {
+            _httpContextAccessor = httpContextAccessor;
             _tokenProvider = tokenProvider;
             this.responseModel = new();
+            VillaApiUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
             this.httpClient = httpClient;
         }
 
@@ -180,6 +186,47 @@ namespace MagicVilla_Web.Services
                 catch (Exception e)
                 {
                     throw;
+                }
+            }
+        }
+
+        #endregion
+
+        #region INVOKE REFRESH TOKEN ENDPOINT
+
+        private async Task InvokeRefreshTokenEndpoint(HttpClient httpClient, string existingAccessToken,
+            string existingRefreshToken)
+        {
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri($"{VillaApiUrl}/api/{SD.CurrentAPIVersion}/UsersAuth/refresh");
+            message.Method = HttpMethod.Post;
+            message.Content = new StringContent(JsonConvert.SerializeObject(new TokenDTO()
+            {
+                AccessToken = existingAccessToken,
+                RefreshToken = existingRefreshToken
+            }), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(message);
+            var content = await response.Content.ReadAsStringAsync();
+            var apiResponse = JsonConvert.DeserializeObject<APIResponse>(content);
+
+            if (apiResponse?.IsSuccess != null)
+            {
+                await _httpContextAccessor.HttpContext.SignOutAsync();
+                _tokenProvider.ClearToken();
+            }
+            else
+            {
+                var tokenDataStr = JsonConvert.SerializeObject(apiResponse.Result);
+                var tokenDto = JsonConvert.DeserializeObject<TokenDTO>(tokenDataStr);
+
+                if (tokenDto != null && !string.IsNullOrEmpty(tokenDto.AccessToken))
+                {
+                    //New method to sign in with the new token that we receive
+
+                    httpClient.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", tokenDto.AccessToken);
                 }
             }
         }
